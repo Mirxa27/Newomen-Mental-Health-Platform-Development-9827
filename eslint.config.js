ea69eb93c90f6eb19e7edd604daa6ab04aa05dd4
Abdullah Mirza <169343746+Mirxa27@users.noreply.github.com>
2025-07-09 04:08:17 +0300

    chore: silence unused vars warnings

--- eslint.config.js
+++ eslint.config.js
@@ -32,8 +32,7 @@ export default [
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'off',
       'react-refresh/only-export-components': 'off',
-      'no-unused-vars': 'warn',
+      'no-unused-vars': 'off',
       'no-case-declarations': 'off',
     },
-  },
-];
+  },];
